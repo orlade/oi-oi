038275eb187b263f4319dbb6d16887c8fdc46099
Oliver Lade <piemaster21@gmail.com>
2017-01-03 22:35:49 +1100

    chore: fix tests, add release task

--- test/index_test.js
+++ test/index_test.js
@@ -1,16 +1,23 @@
 var assert = require('assert'),
-    oi = require('@oi/oi'),
-    pluginFn = require('../index');
+  oi = require('@oi/oi'),
+  pluginFn = require('../index');
+
+const host = {
+  version: '0.1.1',
+  log: require('winston'),
+};
 
 describe("Oi development module", () => {
+
   it("can be loaded", () => {
-    var plugin = pluginFn(null, null, null);
+    var plugin = pluginFn(null, host, null);
     assert.equal(plugin.command, 'oi');
   });
 
   it("can be loaded as a module", () => {
-    var plugin = pluginFn();
+    var plugin = pluginFn(null, host, null);
     var module = new oi.Module(plugin);
     assert.equal(module.command, 'oi');
   });
+
 });
