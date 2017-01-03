dbe12d2ff70e879047f8ed9eb6e45b4cfa9ca790
Oliver Lade <piemaster21@gmail.com>
2017-01-03 22:30:37 +1100

    Update oi, add clone command, minor cleanup

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-var oi = require('@oi/oi');
+const oi = require('@oi/oi');
+const fs = require('fs');
+require('colors');
 
-module.exports = function (data, host, options) {
-  return new oi.Module({
+module.exports = function(data, host, options) {
+  const log = host.log;
+  return {
     command: "oi",
     describe: "Runs development actions on Oi itself",
     actions: {
+      clone: {
+        describe: 'Clone the Oi repository into your workspace',
+        handler: (name, options) => {
+          if (fs.existsSync(options.workingDir)) {
+            log.info(`Repo already exists at ${options.workingDir.magenta}`);
+          } else {
+            log.debug(`Cloning into ${options.workingDir.magenta}...`);
+            oi.utils.exec(`git clone --recursive `
+              + `${options.repo} ${options.workingDir}`);
+          }
+        },
+      },
       build: {
-        describe: "Builds Oi from source",
-        handler: (name, options) => oi.utils.exec('npm run build', options)
+        describe: "Build Oi from source",
+        handler: (name, options) => oi.utils.exec('npm install && npm run build', options),
       },
       test: {
-        describe: "Tests the Oi package",
-        handler: (name, options) => oi.utils.exec('npm test', options)
-      }
+        describe: "Test the Oi package",
+        handler: (name, options) => oi.utils.exec('npm test', options),
+      },
     },
     config: {
-      workingDir: '${workspace}/oi'
+      workingDir: '${workspace}/oi',
     },
     requireConfig: {
-      workspace: true
-    }
-  });
+      workspace: true,
+    },
+  };
 };
